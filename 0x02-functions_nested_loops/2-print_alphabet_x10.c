980f516fae237b5671a119de92e598ac2422d46f
Bereket18 <bereketadamsseged@gmail.com>
2022-07-05 12:01:21 -0700

    hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
  * print_alphabet_x10 -> prints the lowercase alphabets
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int j;
 	int x;
@@ -15,6 +15,6 @@ void print_alphabet_x10(void);
 		{
 			putchar(j);
 		}
-		putchar('\n');
+		 putchar('\n');
 	}
 }
