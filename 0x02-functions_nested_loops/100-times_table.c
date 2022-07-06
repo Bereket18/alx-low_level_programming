ea847e0787784cf2463156728cf19fb72d176c8d
Bereket18 <101464558+Bereket18@users.noreply.github.com>
2022-07-06 10:01:31 +0300

    updateN

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,15 +1,12 @@
 #include "main.h"
+
 /**
- * print_times_table - prints the n times table
- * @n: number times table (0 < n <= 15)
- *
- * Return: no return
- * ?
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
 void print_times_table(int n)
 {
 	int a, b, c;
-
 	if (n >= 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
