82aca52a5338182c0e1f6759f903b63dee0a04b2
Bereket Adamsseged <bereketadamsseged@gmail.com>
2022-07-13 22:32:13 +0300

    103 Advanced

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,31 +1,45 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * rot13 - Write a function that encodes a string using rot13
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
  *
- * @hi: This is my input string
- *
- * Return: String converted to rot13
- *
+ * Return: address of r or 0
  */
-
-char *rot13(char *hi)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int index, count;
-
-	char minus[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char mayus[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-	for (index = 0; hi[index] != '\0'; ++index)
-	{
-		for (count = 0; minus[count] != '\0' ; count++)
-		{
-			if (hi[index] == minus[count])
-			{
-				hi[index] = mayus[count];
-				break;
-			}
-		}
-	}
-	hi[index] = '\0';
-	return (hi);
+int i, j, k, l, m, n;
+for (i = 0; n1[i]; i++)
+;
+for (j = 0; n2[j]; j++)
+;
+if (i > size_r || j > size_r)
+return (0);
+m = 0;
+for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+{
+n = m;
+if (i >= 0)
+n += n1[i] - '0';
+if (j >= 0)
+n += n2[j] - '0';
+if (i < 0 && j < 0 && n == 0)
+{
+break;
+}
+m = n / 10;
+r[k] = n % 10 + '0';
+}
+r[k] = '\0';
+if (i >= 0 || j >= 0 || m)
+return (0);
+for (k -= 1, l = 0; l < k; k--, l++)
+{
+m = r[k];
+r[k] = r[l];
+r[l] = m;
+}
+return (r);
 }
