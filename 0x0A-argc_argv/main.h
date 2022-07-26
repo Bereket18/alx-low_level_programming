1947e915f738c5a37867d01cb1fea7db688b5300
Bereket Adamsseged <bereketadamsseged@gmail.com>
2022-07-26 21:59:07 +0300

    update

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -6,10 +6,6 @@
 #include<ctype.h>
 
 int _putchar(char c);
-<<<<<<< HEAD
-#endif
-=======
 int main(int argc, char *argv[]);
 
 #endif /* MAIN_H */
->>>>>>> 318ca85fddd752d137c90131fb4e8e60dae20a57
