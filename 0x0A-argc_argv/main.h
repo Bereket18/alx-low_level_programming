352d3f8691c7f23774ae42612906d16272d97230
Bereket18 <101464558+Bereket18@users.noreply.github.com>
2022-07-26 21:49:18 +0300

    Update main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,4 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<ctype.h>
+
 int _putchar(char c);
-#endif
+int main(int argc, char *argv[]);
+
+#endif /* MAIN_H */
